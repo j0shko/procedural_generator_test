d06321d08e5bb358a875c162a52e2df5ee262bbf
Josip Tomić <revan811@gmail.com>
2016-02-02 00:50:52 +0100

    heightmap generator with octaves and beginning of island mask

---
+++ Assets/Editor/LandmassGeneratorEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(LandmassGenerator))]
+public class NewBehaviourScript : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        LandmassGenerator generator = (LandmassGenerator)target;
+        if (GUILayout.Button("Generate"))
+        {
+            generator.generateHeightmap();
+        }
+    }
+}
--- Assets/Scripts/LandmassGenerator.cs
+++ Assets/Scripts/LandmassGenerator.cs
@@ -11,53 +11,76 @@ public class LandmassGenerator : MonoBehaviour {
     public int width = 100;
     [Range(1, 1024)]
     public int height = 100;
+    [Range(1, 5)]
+    public int octaveCount = 3;
 
-    public float xOrg;
-    public float yOrg;
+    [Range(0, 0.9f)]
+    public float islandPercentage = 0.8f;
 
-    private float lastRenderTime;
-    public float renderAgainTime = 1f;
+    [Range(0, 0.5f)]
+    public float coastPercentage = 0.2f;
 
-    private Terrain terrain;
+    private float xOrg;
+    private float yOrg;
+
+    private Renderer plane_renderer;
 
 	// Use this for initialization
 	void Start () {
-        lastRenderTime = Time.time;
-        xOrg = Random.Range(0, 500);
-        yOrg = Random.Range(0, 500);
+        plane_renderer = GetComponent<Renderer>();
+        xOrg = Random.Range(0, 1024);
+        yOrg = Random.Range(0, 1024);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Time.time > lastRenderTime + renderAgainTime)
-        {
-            //Debug.Log("Rendering again ...");
-
-            terrain = GetComponent<Terrain>();
-            float[,] heightmap;
-            Texture2D noiseTexture = createNoiseTexture(width, height, lucanarity, persistance, out heightmap);
-            terrain.terrainData.SetHeights(0, 0, heightmap);
-            terrain.Flush();
-
-            lastRenderTime = Time.time;
-        }
+        
     }
 
-    Texture2D createNoiseTexture(int width, int height, float lucanarity, float persistance, out float[,] heightmap)
+    public Texture2D createNoiseTexture()
     {
         Texture2D noiseTex = new Texture2D(width, height);
         Color[] pixels = new Color[width * height];
-        heightmap = new float[height, width];
+
+        float[] pixelIntensity = new float[width * height];
+        for (int octave = 0; octave < octaveCount; octave++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    float xCoord = xOrg + (float)x / width * Mathf.Pow(lucanarity, octave);
+                    float yCoord = yOrg + (float)y / height * Mathf.Pow(lucanarity, octave);
+                    float noise = (Mathf.PerlinNoise(xCoord, yCoord) - 0.5f) * Mathf.Pow(persistance, octave);
+                    pixelIntensity[y * width + x] += noise;
+                }
+            }
+        }
+
+        float radiusX = width / 2f;
+        float radiusy = height / 2f;
+        float tresholdX = islandPercentage * width / 2f;
+        float tresholdY = islandPercentage * height / 2f;
 
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                float xCoord = xOrg + (float) x / width * lucanarity;
-                float yCoord = yOrg + (float) y / height * lucanarity;
-                float noise = Mathf.PerlinNoise(xCoord, yCoord) * persistance;
-                pixels[y * width + x] = new Color(noise, noise, noise, noise);
-                heightmap[y, x] = noise;
+                float centerToX = x - radiusX;
+                float centerToY = y - radiusy;
+
+                float distanceToCenter = Mathf.Sqrt(centerToX * centerToX + centerToY * centerToY);
+
+                float pixel;
+                if (distanceToCenter > tresholdX || distanceToCenter > tresholdY)
+                {
+                    pixel = 0;
+                } else
+                {
+                    pixel = pixelIntensity[y * width + x] + 0.5f;
+                }
+
+                pixels[y * width + x] = new Color(pixel, pixel, pixel);
             }
         }
 
@@ -65,5 +88,16 @@ public class LandmassGenerator : MonoBehaviour {
         noiseTex.Apply();
 
         return noiseTex;
+    }
+
+    public void generateHeightmap()
+    {
+        Debug.Log("Rendering again ...");
+
+        xOrg = Random.Range(0, 500);
+        yOrg = Random.Range(0, 500);
+
+        Texture2D noiseTexture = createNoiseTexture();
+        plane_renderer.material.mainTexture = noiseTexture;
     }
 }
